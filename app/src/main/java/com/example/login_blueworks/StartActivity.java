9a7d5eb8446435b25abf55cb6d979b057f71648f
lolycastro <babu.vivin@gmail.com>
2023-09-20 09:16:41 -0300

    reparacao de erros

--- app/src/main/java/com/example/login_blueworks/StartActivity.java
+++ app/src/main/java/com/example/login_blueworks/StartActivity.java
@@ -24,44 +24,44 @@ public class StartActivity  extends AppCompatActivity implements NavigationView.
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
-        Toolbar mToolbar = findViewById(R.id.toolbar); //ignore red line errors
-        setSupportActionBar(toolbar);
-
-        drawerLayout = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav,
-                R.string.close_nav);
-        drawerLayout.addDrawerListener(toggle);
-        toggle.syncState();
-
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
-            navigationView.setCheckedItem(R.id.nav_home);
-        }
+//        Toolbar mToolbar = findViewById(R.id.toolbar); //ignore red line errors
+//        setSupportActionBar(toolbar);
+//
+//        drawerLayout = findViewById(R.id.drawer_layout);
+//        NavigationView navigationView = findViewById(R.id.nav_view);
+//        navigationView.setNavigationItemSelectedListener(this);
+//
+//        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav,
+//                R.string.close_nav);
+//        drawerLayout.addDrawerListener(toggle);
+//        toggle.syncState();
+//
+//        if (savedInstanceState == null) {
+//            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+//            navigationView.setCheckedItem(R.id.nav_home);
+//        }
     }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()){
-            case R.id.nav_home:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new HomeFragment()).commit();
-                break;
-
-            case R.id.nav_about:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new HomeFragment()).commit();
-                break;
-
-            case R.id.nav_perfil:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new HomeFragment()).commit();
-                break;
-
-            case R.id.nav_logout:
-                Toast.makeText(this,"Logout",Toast.LENGTH_SHORT).show();
-                break;
-
-        }
+//        switch (item.getItemId()){
+//            case R.id.nav_home:
+//                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new HomeFragment()).commit();
+//                break;
+//
+//            case R.id.nav_about:
+//                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new HomeFragment()).commit();
+//                break;
+//
+//            case R.id.nav_perfil:
+//                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new HomeFragment()).commit();
+//                break;
+//
+//            case R.id.nav_logout:
+//                Toast.makeText(this,"Logout",Toast.LENGTH_SHORT).show();
+//                break;
+//
+//        }
 
         drawerLayout.closeDrawer(GravityCompat.START);
         return true;
